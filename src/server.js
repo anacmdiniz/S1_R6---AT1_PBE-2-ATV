3ddd005be95a8e3634858ab7cc0a6e513a51fa9f
ANA CAROLINA MOTA DINIZ <ana.diniz18@senaisp.edu.br>
2026-06-16 13:23:56 -0300

    mudando o server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/routes.js';
+import { initializeDatabase } from './configs/Database.js';
 
 
 import path from 'path';
